06e5d10e537d3d5b42ad9a0ff4f93fbfc72d210f
nefa1996 <76012032+nefa1996@users.noreply.github.com>
2026-01-15 00:35:09 +0100

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,6 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
 TON_WALLET = os.getenv("TON_WALLET")
 MIN_REWARD = float(os.getenv("MIN_REWARD", 1.0))
-LOG_CHANNELS = "@" + os.getenv("LOG_CHANNELS", "").split(",")
+
+# Получаем список каналов и добавляем '@' к каждому элементу
+LOG_CHANNELS = ["@" + ch for ch in os.getenv("LOG_CHANNELS", "").split(",") if ch]
