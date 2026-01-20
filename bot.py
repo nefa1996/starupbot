9f015e7da9b83238ac77630adb4c5b18a97e5c05
nefa1996 <76012032+nefa1996@users.noreply.github.com>
2026-01-20 23:05:22 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -77,7 +77,6 @@ async def admin_all_commands(cb: CallbackQuery):
         "<code>/id</code> — Узнать ID текущего чата\n"
         "<code>/addstars</code> — Тестовое начисление 1000 ⭐ (только супер-админ)\n"
         "<code>/create_check</code> — Создать чек на звезды\n"
-        "<code>/news</code> - Создать пост в группу (только супер-админ)\n"
     )
     await cb.message.answer(text, parse_mode="HTML")
     await cb.answer()
